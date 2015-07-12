cfe68cc59899d77c25f6e77e7b11c12d891f435a
Michael Ficarra <git@michael.ficarra.me>
2015-07-11 23:33:49 -0700

    fixes #29: update to support PureScript 0.7

--- src/Signal.purs
+++ src/Signal.purs
@@ -1,7 +1,7 @@
 module Signal
-  ( Signal(..)
+  ( Signal()
   , constant
-  , map
+  , mapSig
   , applySig
   , merge
   , foldp
@@ -17,168 +17,74 @@ module Signal
   , (~)
   ) where
 
-import Control.Monad.Eff
-import Data.Function
+import Control.Monad.Eff (Eff())
+import Prelude ((<$>), (<*>), flip, Unit(), Eq, Semigroup, Functor, Applicative, Apply)
 
 foreign import data Signal :: * -> *
 
-foreign import constant """
-  function constant(initial) {
-    var subs = [];
-    var val = initial;
-    var sig = {
-      subscribe: function(sub) {
-        subs.push(sub);
-        sub(val);
-      },
-      get: function() { return val; },
-      set: function(newval) {
-        val = newval;
-        subs.forEach(function(sub) { sub(newval); });
-      }
-    };
-    return sig;
-  }""" :: forall a. a -> Signal a
+foreign import constant :: forall a. a -> Signal a
 
-foreign import mapP """
-  function mapP(constant, fun, sig) {
-    var out = constant(fun(sig.get()));
-    sig.subscribe(function(val) { out.set(fun(val)); });
-    return out;
-  }""" :: forall a b c. Fn3 (c -> Signal c) (a -> b) (Signal a) (Signal b)
+foreign import mapSigP :: forall a b c. (c -> Signal c) -> (a -> b) -> (Signal a) -> (Signal b)
 
-map :: forall a b. (a -> b) -> Signal a -> Signal b
-map = runFn3 mapP constant
+mapSig :: forall a b. (a -> b) -> Signal a -> Signal b
+mapSig = mapSigP constant
 
-foreign import applySigP """
-  function applySigP(constant, fun, sig) {
-    var out = constant(fun.get()(sig.get()));
-    var produce = function() { out.set(fun.get()(sig.get())); };
-    fun.subscribe(produce);
-    sig.subscribe(produce);
-    return out;
-  }""" :: forall a b c. Fn3 (c -> Signal c) (Signal (a -> b)) (Signal a) (Signal b)
+foreign import applySigP :: forall a b c. (c -> Signal c) -> (Signal (a -> b)) -> (Signal a) -> (Signal b)
 
 applySig :: forall a b. Signal (a -> b) -> Signal a -> Signal b
-applySig = runFn3 applySigP constant
+applySig = applySigP constant
 
-foreign import mergeP """
-  function mergeP(constant, sig1, sig2) {
-    var out = constant(sig1.get());
-    sig1.subscribe(out.set);
-    sig2.subscribe(out.set);
-    return out;
-  }""" :: forall a c. Fn3 (c -> Signal c) (Signal a) (Signal a) (Signal a)
+foreign import mergeP :: forall a c. (c -> Signal c) -> (Signal a) -> (Signal a) -> (Signal a)
 
 merge :: forall a. Signal a -> Signal a -> Signal a
-merge = runFn3 mergeP constant
+merge = mergeP constant
 
-foreign import foldpP """
-  function foldpP(constant, fun, seed, sig) {
-    var acc = seed;
-    var out = constant(acc);
-    sig.subscribe(function(val) {
-      acc = fun(val)(acc);
-      out.set(acc);
-    });
-    return out;
-  }""" :: forall a b c. Fn4 (c -> Signal c) (a -> b -> b) b (Signal a) (Signal b)
+foreign import foldpP :: forall a b c. (c -> Signal c) -> (a -> b -> b) -> b -> (Signal a) -> (Signal b)
 
 foldp :: forall a b. (a -> b -> b) -> b -> Signal a -> Signal b
-foldp = runFn4 foldpP constant
+foldp = foldpP constant
 
-foreign import sampleOnP """
-  function sampleOnP(constant, sig1, sig2) {
-    var out = constant(sig2.get());
-    sig1.subscribe(function() {
-      out.set(sig2.get());
-    });
-    return out;
-  }""" :: forall a b c. Fn3 (c -> Signal c) (Signal a) (Signal b) (Signal b)
+foreign import sampleOnP :: forall a b c. (c -> Signal c) -> (Signal a) -> (Signal b) -> (Signal b)
 
 sampleOn :: forall a b. Signal a -> Signal b -> Signal b
-sampleOn = runFn3 sampleOnP constant
+sampleOn = sampleOnP constant
 
-foreign import distinctP """
-  function distinctP(eq) {
-  return function(constant) {
-  return function(sig) {
-    var val = sig.get();
-    var out = constant(val);
-    sig.subscribe(function(newval) {
-      if (eq['/='](val)(newval)) {
-        val = newval;
-        out.set(val);
-      }
-    });
-    return out;
-  };};}""" :: forall a c. (Eq a) => (c -> Signal c) -> Signal a -> Signal a
+foreign import distinctP :: forall a c. (Eq a) => (c -> Signal c) -> Signal a -> Signal a
 
 distinct :: forall a. (Eq a) => Signal a -> Signal a
 distinct = distinctP constant
 
-foreign import distinctRefP """
-  function distinctRefP(constant, sig) {
-    var val = sig.get();
-    var out = constant(val);
-    sig.subscribe(function(newval) {
-      if (val !== newval) {
-        val = newval;
-        out.set(val);
-      }
-    });
-    return out;
-  }""" :: forall a c. Fn2 (c -> Signal c) (Signal a) (Signal a)
+foreign import distinctRefP :: forall a c. (c -> Signal c) -> (Signal a) -> (Signal a)
 
 distinct' :: forall a. Signal a -> Signal a
-distinct' = runFn2 distinctRefP constant
+distinct' = distinctRefP constant
 
 zip :: forall a b c. (a -> b -> c) -> Signal a -> Signal b -> Signal c
 zip = map2
 
-foreign import runSignal """
-  function runSignal(sig) {
-    return function() {
-      sig.subscribe(function(val) {
-        val();
-      });
-      return {};
-    };
-  }""" :: forall e. Signal (Eff e Unit) -> Eff e Unit
+foreign import runSignal :: forall e. Signal (Eff e Unit) -> Eff e Unit
 
-foreign import unwrapP """
-  function unwrapP(constant, sig) {
-    return function() {
-      var out = constant(sig.get()());
-      sig.subscribe(function(val) { out.set(val()); });
-      return out;
-    };
-  }""" :: forall e a c. Fn2 (c -> Signal c) (Signal (Eff e a)) (Eff e (Signal a))
+foreign import unwrapP :: forall e a c. (c -> Signal c) -> (Signal (Eff e a)) -> (Eff e (Signal a))
 
 unwrap :: forall a e. Signal (Eff e a) -> Eff e (Signal a)
-unwrap = runFn2 unwrapP constant
+unwrap = unwrapP constant
 
-foreign import keepIfP """
-  function keepIfP(constant, fn, seed, sig) {
-    var out = constant(fn(sig.get()) ? sig.get() : seed);
-    sig.subscribe(function(val) { if (fn(val)) out.set(val); });
-    return out;
-  }""" :: forall a c. Fn4 (c -> Signal c) (a -> Boolean) a (Signal a) (Signal a)
+foreign import keepIfP :: forall a c. (c -> Signal c) -> (a -> Boolean) -> a -> (Signal a) -> (Signal a)
 
 keepIf :: forall a. (a -> Boolean) -> a -> Signal a -> Signal a
-keepIf = runFn4 keepIfP constant
+keepIf = keepIfP constant
 
 instance functorSignal :: Functor Signal where
-  (<$>) = map
+  map = mapSig
 
 instance applySignal :: Apply Signal where
-  (<*>) = applySig
+  apply = applySig
 
 instance applicativeSignal :: Applicative Signal where
   pure = constant
 
 instance semigroupSignal :: Semigroup (Signal a) where
-  (<>) = merge
+  append = merge
 
 infixl 4 <~
 (<~) :: forall f a b. (Functor f) => (a -> b) -> f a -> f b
