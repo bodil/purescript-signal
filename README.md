e77dd30948e1afda71bd70a2c74fd0455118e379
John Watson <john.watson@gmx.co.uk>
2018-05-30 10:46:06 +0100

    Update for PureScript 0.12

--- README.md
+++ README.md
@@ -6,28 +6,28 @@ See [the Elm documentation](http://elm-lang.org:1234/guide/reactivity#signals) f
 
 ## PureScript Usage Patterns
 
-PureScript depends on effects (specifically, the `Eff` monad) to manage side effects, where Elm's runtime generally manages them for you. `purescript-signal` provides the `Signal.runSignal` function for running effectful signals.
+PureScript depends on effects (specifically, the `Effect` monad) to manage side effects, where Elm's runtime generally manages them for you. `purescript-signal` provides the `Signal.runSignal` function for running effectful signals.
 
 ```purescript
 module Main where
 
-import Control.Monad.Eff.Console
-import Control.Monad.Eff
+import Effect.Console
+import Effect (Effect)
 import Prelude
 import Signal
 
 hello :: Signal String
 hello = constant "Hello Joe!"
 
-helloEffect :: forall eff. Signal (Eff (console :: CONSOLE | eff) Unit)
+helloEffect :: Signal (Effect Unit)
 helloEffect = hello ~> log
 
 main = runSignal helloEffect
 ```
 
-This simple example takes a constant signal which contains the string `"Hello Joe!"` and maps it over the `Control.Monad.Eff.Console.log` function, which has the type `forall eff. String -> Eff (console :: CONSOLE | eff) Unit`, thus taking the `String` content of the signal and turning it into an effect which logs the provided string to the user's console.
+This simple example takes a constant signal which contains the string `"Hello Joe!"` and maps it over the `Effect.Console.log` function, which has the type `String -> Effect Unit`, thus taking the `String` content of the signal and turning it into an effect which logs the provided string to the user's console.
 
-This gives us a `Signal (Eff (console :: CONSOLE | eff) Unit)`. We use `runSignal` to take the signal of effects and run each effect in turn—in our case, just the one effect which prints `"Hello Joe!"` to the console.
+This gives us a `Signal (Effect Unit)`. We use `runSignal` to take the signal of effects and run each effect in turn—in our case, just the one effect which prints `"Hello Joe!"` to the console.
 
 ## API Documentation
 
--- src/Signal/DOM.purs
+++ src/Signal/DOM.purs
@@ -11,9 +11,8 @@ module Signal.DOM
   , Touch(..)
   ) where
 
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Timer (TIMER)
-import DOM (DOM)
+
+import Effect (Effect)
 import Prelude (($), bind, pure)
 import Signal (constant, Signal, (~>))
 import Signal.Time (now, Time)
@@ -21,18 +20,18 @@ import Signal.Time (now, Time)
 type CoordinatePair = { x :: Int, y :: Int }
 type DimensionPair  = { w :: Int, h :: Int }
 
-foreign import keyPressedP :: forall e c. (c -> Signal c) -> Int -> Eff (dom :: DOM | e) (Signal Boolean)
+foreign import keyPressedP :: forall c. (c -> Signal c) -> Int -> Effect (Signal Boolean)
 
 -- |Creates a signal which will be `true` when the key matching the given key
 -- |code is pressed, and `false` when it's released.
-keyPressed :: forall e. Int -> Eff (dom :: DOM | e) (Signal Boolean)
+keyPressed :: Int -> Effect (Signal Boolean)
 keyPressed = keyPressedP constant
 
-foreign import mouseButtonP :: forall e c. (c -> Signal c) -> Int -> Eff (dom :: DOM | e) (Signal Boolean)
+foreign import mouseButtonP :: forall c. (c -> Signal c) -> Int -> Effect (Signal Boolean)
 
 -- |Creates a signal which will be `true` when the given mouse button is
 -- |pressed, and `false` when it's released.
-mouseButton :: forall e. Int -> Eff (dom :: DOM | e) (Signal Boolean)
+mouseButton :: Int -> Effect(Signal Boolean)
 mouseButton = mouseButtonP constant
 
 type Touch = { id :: String
@@ -42,37 +41,37 @@ type Touch = { id :: String
              , radiusX :: Int, radiusY :: Int
              , rotationAngle :: Number, force :: Number }
 
-foreign import touchP :: forall e c. (c -> Signal c) -> Eff (dom :: DOM | e) (Signal (Array Touch))
+foreign import touchP :: forall c. (c -> Signal c) -> Effect (Signal (Array Touch))
 
 -- |A signal containing the current state of the touch device, as described by
 -- |the `Touch` record type.
-touch :: forall e. Eff (dom :: DOM | e) (Signal (Array Touch))
+touch :: Effect (Signal (Array Touch))
 touch = touchP constant
 
 -- |A signal which will be `true` when at least one finger is touching the
 -- |touch device, and `false` otherwise.
-tap :: forall e. Eff (dom :: DOM | e) (Signal Boolean)
+tap :: Effect (Signal Boolean)
 tap = do
   touches <- touch
   pure $ touches ~> \t -> case t of
     [] -> false
     _ -> true
 
-foreign import mousePosP :: forall e c. (c -> Signal c) -> Eff (dom :: DOM | e) (Signal CoordinatePair)
+foreign import mousePosP :: forall c. (c -> Signal c) -> Effect (Signal CoordinatePair)
 
 -- |A signal containing the current mouse position.
-mousePos :: forall e. Eff (dom :: DOM | e) (Signal CoordinatePair)
+mousePos :: Effect (Signal CoordinatePair)
 mousePos = mousePosP constant
 
-foreign import animationFrameP :: forall e c. (c -> Signal c) -> Eff (timer :: TIMER | e) Time -> Eff (dom :: DOM, timer :: TIMER | e) (Signal Time)
+foreign import animationFrameP :: forall c. (c -> Signal c) -> Effect Time -> Effect (Signal Time)
 
 -- |A signal which yields the current time, as determined by `now`, on every
 -- |animation frame (see [https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame]).
-animationFrame :: forall e. Eff (dom :: DOM, timer :: TIMER | e) (Signal Time)
+animationFrame :: Effect (Signal Time)
 animationFrame = animationFrameP constant now
 
-foreign import windowDimensionsP :: forall e c. (c -> Signal c) -> Eff (dom :: DOM | e) (Signal DimensionPair)
+foreign import windowDimensionsP :: forall c. (c -> Signal c) -> Effect (Signal DimensionPair)
 
 -- |A signal which contains the document window's current width and height.
-windowDimensions :: forall e. Eff (dom :: DOM | e) (Signal DimensionPair)
+windowDimensions :: Effect (Signal DimensionPair)
 windowDimensions = windowDimensionsP constant
